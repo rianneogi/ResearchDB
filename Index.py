a148a8111e4c379d9db15a8872579ded7e01d669
Rian Neogi <rianneogi@gmail.com>
2022-08-01 19:15:05 -0400

    fixed a crucial bug

--- Index.py
+++ Index.py
@@ -428,10 +428,10 @@ def add_paper_by_corpus_id(corpus_id):
 					# else:
 					# 	path += 'CorpusID:' + corpus_id+'.pdf'
 
-					# print(json['authors'])
-					path += getAuthorsInitials(json['authors']) + ' ' + json['title']+'.pdf'
-					path = path.replace('/', '')
-					path = path.replace('\\', '')
+					filename = getAuthorsInitials(json['authors']) + ' ' + json['title']+'.pdf'
+					filename = filename.replace('/', '')
+					filename = filename.replace('\\', '')
+					path += filename
 
 					if os.path.exists(path):
 						path = path[:-4] + ' CorpusID:' + corpus_id+'.pdf' #remove .pdf and add corpus_id.pdf
